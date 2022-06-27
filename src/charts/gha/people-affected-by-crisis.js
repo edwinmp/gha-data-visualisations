22667bb9df78ded54ae820297acd777b68741552
ThatcherK <kaliithatmau@gmail.com>
2022-06-27 11:40:55 +0300

    Refactor and mmatch csv country names to geojson names

--- src/charts/gha/people-affected-by-crisis.js
+++ src/charts/gha/people-affected-by-crisis.js
@@ -1,6 +1,46 @@
 import fetchCSVData from '../../utils/data';
 
 const MAP_FILE_PATH = 'src/charts/data/world_map.geo.json';
+
+const matchCountryNames = (csvData, worldData) => {
+  const matchedData = csvData.map((stream) => {
+    const countryObject = worldData.features.find(
+      (feature) => feature.properties.iso_a3 === stream.Country_ID,
+    );
+    if (countryObject) {
+      // eslint-disable-next-line no-param-reassign
+      stream.Country_name = countryObject.properties.name;
+    }
+
+    return stream;
+  });
+
+  return matchedData;
+};
+const getVariableSpecificData = (dataArray, variableName) => (
+  dataArray.map((values) => ({
+    name: values.name,
+    value: values[variableName],
+  })));
+
+const processedData = (countries, processedCountryData) => {
+  const data = [];
+  // eslint-disable-next-line array-callback-return
+  countries.map((country) => {
+    const countryData = {};
+    countryData.name = country;
+    // eslint-disable-next-line array-callback-return
+    processedCountryData.map((stream) => {
+      if (stream.Country_name === country) {
+        countryData[stream.variable] = stream.value;
+      }
+    });
+    data.push(countryData);
+  });
+
+  return data;
+};
+
 const renderPeopleAffectedByCrisisMap = () => {
   window.DICharts.handler.addChart({
     className: 'dicharts--gha-people-affected-by-crisis',
@@ -15,18 +55,22 @@ const renderPeopleAffectedByCrisisMap = () => {
            *
            * const chart = window.echarts.init(chartNode);
            */
-          const csv = 'https://raw.githubusercontent.com/devinit/di-chart-boilerplate/gha/2021/charts/public/assets/data/GHA/2021/interactivity-donors.csv';
+          const csv = 'src/charts/data/map_data_long.csv';
           fetchCSVData(csv).then((data) => {
             // create UI elements
-            console.log(data);
+            const variable = 'Severity_score';
 
             const chart = window.echarts.init(chartNode);
 
             dichart.showLoading();
             window.$.getJSON(MAP_FILE_PATH, (worldJson) => {
+              const procesedCountryNameData = matchCountryNames(data, worldJson);
+              const countries = [
+                ...new Set(procesedCountryNameData.map((stream) => stream.Country_name)),
+              ];
+              const groupedData = processedData(countries, procesedCountryNameData);
               dichart.hideLoading();
               window.echarts.registerMap('WORLD', worldJson);
-              console.log(worldJson);
               const option = {
                 title: {
                   text: 'People affected by crisis',
@@ -36,9 +80,33 @@ const renderPeopleAffectedByCrisisMap = () => {
                     fontSize: 20,
                   },
                 },
+                tooltip: {},
+                visualMap: {
+                  type: 'piecewise',
+                  calculable: true,
+                  pieces: [
+                    {
+                      min: 5, max: 5, label: '5 - Very High', color: '#6a033b',
+                    },
+                    {
+                      min: 4, max: 4, label: '4 - High', color: '#a71a67',
+                    },
+                    {
+                      min: 3, max: 3, label: '3 - Medium', color: '#bc4084',
+                    },
+                    {
+                      min: 2, max: 2, label: '2 - Low', color: '#e288b9',
+                    },
+                    {
+                      min: 1, max: 1, label: '1 - Very Low', color: '#e6a9ca',
+                    },
+                  ],
+                  selectedMode: false,
+                  orient: 'horizontal',
+                },
                 series: [
                   {
-                    name: 'Severity score',
+                    name: variable,
                     type: 'map',
                     roam: true,
                     map: 'WORLD',
@@ -48,23 +116,7 @@ const renderPeopleAffectedByCrisisMap = () => {
                       },
                     },
                     showLegendSymbol: false,
-                    data: [
-                      { name: 'Yemen', value: 4 },
-                      { name: 'Dem. Rep. Congo', value: 4 },
-                      { name: 'Venezuela', value: 3 },
-                      { name: 'Afghanistan', value: 4 },
-                      { name: 'Syria', value: 5 },
-                      { name: 'Dem. Rep. Korea', value: 4 },
-                      { name: 'Nigeria', value: 4 },
-                      { name: 'Sudan', value: 5 },
-                      { name: 'Colombia', value: 4 },
-                      { name: 'Ethiopia', value: 5 },
-                      { name: 'S. Sudan', value: 4 },
-                      { name: 'Zimbabwe', value: 3 },
-                      { name: 'Chad', value: 4 },
-                      { name: 'Somalia', value: 5 },
-                      { name: 'Iraq', value: 4 },
-                    ],
+                    data: getVariableSpecificData(groupedData, variable),
                   },
                 ],
               };
