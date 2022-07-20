74a8aa37994fbdba32e31e183002d2f56fa270d9
ThatcherK <kaliithatmau@gmail.com>
2022-07-20 11:41:34 +0300

    Remove echartsmap

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import 'core-js';
 import renderDonorsChart from './charts/donors';
 import renderFundingChannelsChart from './charts/funding-channels';
 import renderRecipientChart from './charts/recipients';
-import renderPeopleAffectedByCrisisMap from './charts/people-affected-by-crisis';
-import './styles/styles.css';
 import renderPeopleAffectedByCrisisLeaflet from './charts/leaflet-map';
+import './styles/styles.css';
+
 // import d3 from 'd3'; // eslint-disable-line import/no-unresolved
 
 // Your Code Goes Here i.e. functions
@@ -16,6 +16,5 @@ window.addEventListener('load', () => {
   renderDonorsChart();
   renderFundingChannelsChart();
   renderRecipientChart();
-  renderPeopleAffectedByCrisisMap();
   renderPeopleAffectedByCrisisLeaflet();
 });
