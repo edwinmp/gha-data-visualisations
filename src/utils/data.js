2446d994b6fb457380b2f322092fe687d3890fb1
ThatcherK <kaliithatmau@gmail.com>
2022-07-21 08:39:44 +0300

    Change main branch

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,6 @@
 import { parse } from 'papaparse';
 
-export const ACTIVE_BRANCH = 'main';
+export const ACTIVE_BRANCH = 'feature/interactive-map-default-view';
 
 const fetchCSVData = (url) =>
   new Promise((resolve) => {
