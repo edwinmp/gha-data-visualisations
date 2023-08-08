3e850606059e39087c74a4cb352662d750eaa5c6
ThatcherK <kaliithatmau@gmail.com>
2023-08-08 14:07:49 +0300

    change default branch

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,6 @@
 import { parse } from 'papaparse';
 
-export const ACTIVE_BRANCH = 'dev';
+export const ACTIVE_BRANCH = 'feature/climate-finance';
 
 const fetchCSVData = (url) =>
   new Promise((resolve) => {
