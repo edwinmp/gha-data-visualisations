3935f9575008c5fe573500c91c0326eb42a8a371
ThatcherK <kaliithatmau@gmail.com>
2023-08-24 16:51:32 +0300

    Build files

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,6 @@
 import { parse } from 'papaparse';
 
-export const ACTIVE_BRANCH = 'dev';
+export const ACTIVE_BRANCH = 'feature/climate-map';
 
 const fetchCSVData = (url) =>
   new Promise((resolve) => {
