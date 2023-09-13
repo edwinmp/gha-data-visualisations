b54e3a137770b44730eb95adaf1af05ea6058a71
ThatcherK <kaliithatmau@gmail.com>
2023-09-13 13:18:27 +0300

    Make crisis checkbox checked by default

--- src/components/CheckboxInput.js
+++ src/components/CheckboxInput.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const CheckboxInput = ({ label, onChange }) => {
-  const [checkedYes, setCheckedYes] = useState(false)
+  const [checkedYes, setCheckedYes] = useState(true)
   const [value, setValue] = useState('');
 
   useEffect(()=> {
